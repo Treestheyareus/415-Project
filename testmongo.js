800b597d0710c2f9e868c0494bbe6abc32b0ef7f
Treestheyareus <37349490+Treestheyareus@users.noreply.github.com>
2023-03-28 11:48:21 -0500

    Add proper database name and credentials.

--- testmongo.js
+++ testmongo.js
@@ -1,14 +1,14 @@
 const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://<username>:<pasword>@ckmdb.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://mills415wrany:D88whBQ9NYyYgcS@ckmdb.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri);
 
 async function run() {
   try {
-    const database = client.db('ckmdb');
-    const parts = database.collection('cmps415');
+    const database = client.db('CMPS415');
+    const parts = database.collection('415Parts');
 
     // Query for a part that has partID '12345'
     const query = { partID: '12345' };
