8c9b85beb82fd17b8de23a6700ff69e87da4e2a0
Treestheyareus <37349490+Treestheyareus@users.noreply.github.com>
2023-03-28 12:21:31 -0500

    Oops |
    Add .gt9j9mr. to uri

--- testmongo.js
+++ testmongo.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://mills415wrany:D88whBQ9NYyYgcS@cmps415.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://mills415wrany:D88whBQ9NYyYgcS@cmps415.gt9j9mr.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri);
 
