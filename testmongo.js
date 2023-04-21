6d541c975f7ca58199d81c5336e9890f6dc8832f
Glen <glen.mills@selu.edu>
2023-04-21 00:49:10 -0500

    struggling

--- testmongo.js
+++ testmongo.js
@@ -56,9 +56,7 @@ app.get('/rest/ticket/:id', function(req, res) {
     //Code is identical to finding all.
     //Except for the search term between brackets in the line below.
     console.log("Search term is: " + search_id);
-    var query = {id:0};
-    query[id] = search_id;
-    response = await tickets.findOne(query);
+    response = await tickets.findOne({ id:[search_id] });
     await client.close();
     console.log("Response is...");
     console.log(response);
